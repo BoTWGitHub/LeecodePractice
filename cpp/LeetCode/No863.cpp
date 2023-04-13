83b167af46cb780eb54ed577388e8c257d754260
BoLin <jerry084@yahoo.com.tw>
2023-04-13 17:49:56 +0800

    Add No863

--- cpp/LeetCode/No863.cpp
+++ cpp/LeetCode/No863.cpp
@@ -6,6 +6,8 @@ You can return the answer in any order.
 */
 #include <iostream>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
 #include "Debug\VectorDebug.h"
 #include "Debug\TreeNode.h"
 using namespace std;
@@ -13,72 +15,54 @@ using namespace std;
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int> res = distanceKFromTarget(target, k);
-        if(root!=target && k!=0){
-            int left = findNode(root->left, target);
-            if(left==-1){
-                int right = findNode(root->right, target);
-                if(right>k){
-                    vector<int> temp = distanceKFromTarget(root->right, right-k-1);
-                    res.insert(res.end(), temp.begin(), temp.end());
-                }
-                else if(right<k){
-                    vector<int> temp = distanceKFromTarget(root->left, k-right-1);
-                    res.insert(res.end(), temp.begin(), temp.end());
-                }
-                else{
-                    res.push_back(root->val);
+        unordered_map<TreeNode*, TreeNode*> parentOf;
+        vector<int> res;
+        parentOf[root] = nullptr;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node->left){
+                parentOf[node->left] = node;
+                q.push(node->left);
+            }
+            if(node->right){
+                parentOf[node->right] = node;
+                q.push(node->right);
+            }
+        }
+        int dis = 0;
+        unordered_set<int> seen;
+        seen.insert(target->val);
+        q.push(target);
+        while(!q.empty()){
+            if(dis==k){
+                while(!q.empty()){
+                    res.push_back(q.front()->val);
+                    q.pop();
                 }
             }
             else{
-                if(left>k){
-                    vector<int> temp = distanceKFromTarget(root->left, left-k-1);
-                    res.insert(res.end(), temp.begin(), temp.end());
-                }
-                else if(left<k){
-                    vector<int> temp = distanceKFromTarget(root->right, k-left-1);
-                    res.insert(res.end(), temp.begin(), temp.end());
-                }
-                else{
-                    res.push_back(root->val);
+                int size = q.size();
+                for(int i=0;i<size;i++){
+                    TreeNode* node = q.front();
+                    q.pop();
+                    if(node->left && seen.count(node->left->val)==0){
+                        seen.insert(node->left->val);
+                        q.push(node->left);
+                    }
+                    if(node->right && seen.count(node->right->val)==0){
+                        seen.insert(node->right->val);
+                        q.push(node->right);
+                    }
+                    if(parentOf[node] && seen.count(parentOf[node]->val)==0){
+                        seen.insert(parentOf[node]->val);
+                        q.push(parentOf[node]);
+                    }
                 }
             }
-        }
-        
-        return res;
-    }
-private:
-    int findNode(TreeNode* node, TreeNode* target){
-        if(!node) return -1;
-        if(node==target) return 1;
-        int left = findNode(node->left, target);
-        int right = findNode(node->right, target);
-        if(left!=-1) return left+1;
-        if(right!=-1) return right+1;
-        return -1;
-    }
-    vector<int> distanceKFromTarget(TreeNode* target, int k){
-        if(!target) return {};
-        if(k==0) return {target->val};
-        queue<TreeNode*> q;
-        q.push(target);
-        int dis = 0;
-        vector<int> res;
-        while(!q.empty()){
-            int size = q.size();
             dis++;
-            for(int i=0;i<size;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(dis==k){
-                    if(node->left) res.push_back(node->left->val);
-                    if(node->right) res.push_back(node->right->val);
-                }
-                else{
-                    if(node->left) q.push(node->left);
-                    if(node->right) q.push(node->right);
-                }
-            }
         }
         return res;
     }
@@ -91,7 +75,7 @@ int main(void){
     int k;
     vector<int> res;
 
-    /*target = new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4)));
+    target = new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4)));
     root = new TreeNode(3, target, new TreeNode(1, new TreeNode(0), new TreeNode(8)));
     k = 2;
     res = solution.distanceK(root, target, k);
@@ -101,7 +85,7 @@ int main(void){
     root = target;
     k = 3;
     res = solution.distanceK(root, target, k);
-    printVector(res);*/
+    printVector(res);
 
     target = new TreeNode(1, nullptr, new TreeNode(2, nullptr, new TreeNode(3)));
     root = new TreeNode(0, nullptr, target);
