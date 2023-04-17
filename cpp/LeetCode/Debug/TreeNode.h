fa1177cfbec136835b42aa06684187bef7e6cf73
BoLin <jerry084@yahoo.com.tw>
2023-04-17 11:00:29 +0800

    Move printTree outside the stuct

--- cpp/LeetCode/Debug/TreeNode.h
+++ cpp/LeetCode/Debug/TreeNode.h
@@ -10,26 +10,25 @@ struct TreeNode {
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    
-    void printTree(){
-        TreeNode* node = this;
-        queue<TreeNode*> q;
-        q.push(node);
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* cur = q.front();
-                q.pop();
-                if(cur){
-                    cout << cur->val << ",";
-                    q.push(cur->left);
-                    q.push(cur->right);
-                }
-                else{
-                    cout << "None,";
-                }
-            }
-            cout << endl;
-        }
-    }
 };
+
+void printTree(TreeNode* root){
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        for(int i=0;i<size;i++){
+            TreeNode* cur = q.front();
+            q.pop();
+            if(cur){
+                cout << cur->val << ",";
+                q.push(cur->left);
+                q.push(cur->right);
+            }
+            else{
+                cout << "None,";
+            }
+        }
+        cout << endl;
+    }
+}
